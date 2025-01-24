00362eacfd17a8665697c39e59124d831dd6fe40
Kreuder <daniel.kreuder@htwg-konstanz.de>
2025-01-24 14:09:19 +0100

    Neues Leaderboard

--- leaderBoardLoaderRain.js
+++ leaderBoardLoaderRain.js
@@ -41,8 +41,8 @@ function getSheetData() {
         }).then((response) => {
             const data = response.result.values;
             if (data.length > 0) {
-                // Nur die Spalten C (2), D (3), F (5) und G (6) extrahieren, ab der zweiten Zeile
-                const filteredData = data.slice(1).map(row => [
+                // Nur die Spalten C (2), D (3), F (5) und G (6) extrahieren, ab der zweiten Zeile 
+                const filteredData = data.slice(2).map(row => [
                     row[2],  // Spalte C Name
                     row[3],  // Spalte D Rang
                     formatNumber(row[5]),  // Spalte F mit Formatierung Price
